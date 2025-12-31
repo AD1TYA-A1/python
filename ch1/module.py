af07ab9ff1e77aa34a78873847a22e65138434c9
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2025-12-31 15:24:46 +0530

    Please Hoja Add ch1 Folder 12thTRY on 31stDec2025

---
+++ ch1/module.py
@@ -0,0 +1,13 @@
+# pip install pyjokes
+
+# pip is a python pakage installer or manager that allows us to install various Modules.
+
+
+# Modules are set of code designed to perform a specefic task.For example to calculate mean of numbers we use a module (this module can be installed by running a pip commmand on terminal) that has inbuilt code to peform this task.
+
+# HERE I HAVE USED pyJokes module that allows us to get jokes!!!
+
+import pyjokes
+
+joke  = pyjokes.get_joke();
+print(joke);
