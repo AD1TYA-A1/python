511b3d04e3ffdb1e632740b5ca9d6befd495aa3d
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2026-01-03 21:05:22 +0530

    Adding ch6 Folder with conditionals.py file

---
+++ ch6/conditonals.py
@@ -0,0 +1,12 @@
+age = int(input("Enter Your Age :"))
+# if (age >= 18): IS ====== TO if age >= 18:
+
+if age >= 18:
+    print("You can Vote")
+    print("Goo Ahead!!!")
+elif age <= 0:
+    print("Do not Add Neatuve Age")
+else:
+    print("You Cannot Vote")
+
+print("Program Ended")
