e5ed562f6345741f72852d6d2a7bb77002d2ef53
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2026-01-05 18:33:36 +0530

    Add display list using while loop example

---
+++ ch7/02_displayListUsingWhileLoop.py
@@ -0,0 +1,7 @@
+# Program To print the content of a list sunig while Loops
+l = ["Admin", 23, True, 90.5, "Aditya", False]
+index = 0
+print("Your List contains the following DATA : ")
+while index != len(l):
+    print(l[index])
+    index += 1
