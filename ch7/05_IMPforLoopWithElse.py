4a2aabff25bf1dd3d3f47409aa3b5e2ceffe0d9c
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2026-01-05 18:32:32 +0530

    Remove ch7 to recommit individually

--- ch7/05_IMPforLoopWithElse.py
+++
@@ -1,5 +0,0 @@
-l = ["Admin", 23, True, 90.5, "Aditya", False]
-for i in l:
-    print(i)
-else:       # This else runs when the for Loop Exhausts
-    print("For Loop is completed i.e, For Loop condition Has became FALSE") 
