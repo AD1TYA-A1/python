6d7d9e77340eb42609e51af8b03bd5c27b8323ba
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2026-01-05 18:17:30 +0530

    Adding ch7 Folder that talks about Loops in python

---
+++ ch7/04_iterationUsingForLoop.py
@@ -0,0 +1,30 @@
+# For Loop iteration in List
+l = ["Admin", 23, True, 90.5, "Aditya", False]
+
+for i in l:
+    print(i)
+
+# for i in range(0,len(l)):
+#     print(l[i])
+
+print()
+
+# For Loop iteration in Tupple
+t = (32, "True", True, 90.7, "Aditya")
+for i in t:
+    print(i)
+
+print()
+print()
+
+# For Loop iteration in Dictionery
+dic = {"key": "value", "anotherKey": 23,4:"You are 4"}
+for i in dic:
+    print("Key:",i)
+    print("Value:",dic[i])
+
+
+# For Loop iteration in Strings
+str = "Aditya Gaur"
+for i in str:
+    print(i)
