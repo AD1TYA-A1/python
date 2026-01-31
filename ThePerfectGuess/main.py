3d9489004bad84432e735ca3f79b9f293c3f8882
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2026-01-31 19:30:58 +0530

    Starting a new Project

---
+++ ThePerfectGuess/main.py
@@ -0,0 +1,8 @@
+# We are going to write a program that genrates a random number and asks the user to guess it .
+
+# If the player's guess is higher than the actual number, the program displays "Lower Number Please". Similarly if the user's guess is too low, the program prints "Higher Number Please". When user guesses the correct number, the program displays the number of guesses the player took to arive at the number 
+
+
+# Then The turn Shifts to the another person and the one who took the least amount of guesses WINS!! 
+
+# HINT: Use the random Module
