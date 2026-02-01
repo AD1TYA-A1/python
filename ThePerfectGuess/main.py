6413221d786acdf00c2f8343e2e1ac6670b7f54f
AD1TYA-A1 <a.d.i.t.y.a.654a@gmail.com>
2026-02-01 11:58:31 +0530

    Completed Project 2

--- ThePerfectGuess/main.py
+++ ThePerfectGuess/main.py
@@ -1,8 +1,34 @@
 # We are going to write a program that genrates a random number and asks the user to guess it .
 
-# If the player's guess is higher than the actual number, the program displays "Lower Number Please". Similarly if the user's guess is too low, the program prints "Higher Number Please". When user guesses the correct number, the program displays the number of guesses the player took to arive at the number 
+# If the player's guess is higher than the actual number, the program displays "Lower Number Please". Similarly if the user's guess is too low, the program prints "Higher Number Please". When user guesses the correct number, the program displays the number of guesses the player took to arive at the number
 
 
-# Then The turn Shifts to the another person and the one who took the least amount of guesses WINS!! 
+# Then The turn Shifts to the another person and the one who took the least amount of guesses WINS!!
 
 # HINT: Use the random Module
+
+import random
+
+
+def rndm():
+    # print("I am random function")
+    num = random.randint(1, 100)
+    return num
+
+
+score = 0
+print("Computer have chosen a Number \t TAKE GUESSES!!")
+a = int(input("Guess a number: "))
+rndm = rndm()
+while a != rndm:
+    if a > rndm:
+        print("Lower Number Please!!")
+        score+=1
+    elif a < rndm:
+        print("Higher Number Please!!")
+        score+=1
+
+    a = int(input())
+
+print("Got That")
+print(f"Score: {score}")
